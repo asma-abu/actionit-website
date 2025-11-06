18bbbd5d2e4d64ebcd29149c0550dd2732c7ea89
asma-abu <asmae.abu@gmail.com>
2025-11-05 20:41:46 -0500

    add Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,16 @@
 // app/page.tsx
-"use client"; // ✅ This makes it a Client Component
+"use client";
+import HeroSection from "../components/Herosection";
 
-import React from "react";
+ // ✅ This makes it a Client Component
 
 export default function HomePage() {
   return (
-    <main
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-        fontFamily: "sans-serif",
-        backgroundColor: "#f0f0f0",
-        color: "#333",
-      }}
-    >
-      <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>
-        🎉 Hello, Next.js!
-      </h1>
-      <p style={{ fontSize: "1.2rem", marginBottom: "2rem" }}>
-        Your deployment is working correctly.
-      </p>
-      <button
-        style={{
-          padding: "0.8rem 1.5rem",
-          fontSize: "1rem",
-          backgroundColor: "#6200ee",
-          color: "#fff",
-          border: "none",
-          borderRadius: "8px",
-          cursor: "pointer",
-        }}
-        onClick={() => alert("Button works! ✅")}
-      >
-        Click Me
-      </button>
+    <main>
+      <HeroSection />
     </main>
   );
 }
+
+
+
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flowbite.s3.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flowbite.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
