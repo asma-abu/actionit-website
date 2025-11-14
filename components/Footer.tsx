08667e4f1b33ca2ce595ad55c1487cd0641d59c3
asma-abu <asmae.abu@gmail.com>
2025-11-13 20:29:21 -0500

    footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,9 +26,6 @@ const solutionsLinks = [
 
 const companyLinks = [
   { href: '/about', label: 'About Action.IT' },
-  { href: '/careers', label: 'Careers' },
-  { href: '/partners', label: 'Partners' },
-  { href: '/security', label: 'Security' },
   { href: '/contact', label: 'Contact Us' },
   { href: '/privacy', label: 'Privacy Policy' },
   { href: '/terms', label: 'Terms of Use' },
@@ -52,30 +49,10 @@ function Footer() {
                 Ottawa, ON K1K 4R4, Canada
               </address>
               
-              <div className="max-w-xl lg:max-w-lg mt-8 w-2/3">
-                <h2 className="text-4xl font-semibold tracking-tight !text-white">Subscribe to our newsletter</h2>
-                <div className="mt-6 flex max-w-md gap-x-4">
-                  <label htmlFor="email-address" className="sr-only">
-                    Email address
-                  </label>
-                  <input
-                    id="email-address"
-                    name="email"
-                    type="email"
-                    required
-                    placeholder="Enter your email"
-                    autoComplete="email"
-                    className="min-w-0 flex-auto rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                  />
-                  <button
-                    type="submit"
-                    className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                  >
-                    Subscribe
-                  </button>
-                </div>
-              </div>
+
             </div>
+
+            
             <div>
               <h3 className="text-sm font-semibold !text-white">
                 Company
